8123a5cb5b949d7b5a3be817ea4fa4dff552f774
Tomasz Fijalkowski <3066563+tfij@users.noreply.github.com>
2022-09-25 21:50:42 +0200

    Maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java
+    `maven-publish`
+    signing
     id("pl.allegro.tech.build.axion-release") version "1.14.1"
 }
 
 group = "pl.tfij"
-scmVersion {
-    versionCreator("versionWithBranch")
-}
+project.version = scmVersion.version
 
 repositories {
     mavenCentral()
@@ -21,4 +21,84 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("jar") {
+    manifest {
+        attributes["Implementation-Title"] = project.name
+        attributes["Implementation-Version"] = project.version
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("sonatype") {
+            artifactId = "check-tfij-style"
+            from(components.getByName("java"))
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionOf("");
+                }
+            }
+            pom {
+                name.set("check-tfij-style")
+                description.set("A set of additional check to use with checkstyle.")
+                url.set("https://github.com/tfij/check-tfij-style")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("tfij")
+                        name.set("Tomasz Fijakowski")
+                        email.set("tomasz@chi.pl")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/tfij/check-tfij-style.git")
+                    developerConnection.set("scm:git:https://github.com/tfij/check-tfij-style.git")
+                    url.set("https://github.com/tfij/check-tfij-style.git")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
+}
+
+if (System.getenv().containsKey("SIGNING_GPG_KEY_ID")) {
+    signing {
+        useInMemoryPgpKeys(
+            System.getenv("SIGNING_GPG_KEY_ID"),
+            System.getenv("SIGNING_GPG_PRIVATE_KEY"),
+            System.getenv("SIGNING_GPG_PRIVATE_KEY_PASSWORD")
+        )
+        sign(publishing.publications.getByName("sonatype"))
+    }
+}
+
+tasks.getByName<Javadoc>("javadoc") {
+    if(JavaVersion.current().isJava9Compatible) {
+        (options as? StandardJavadocDocletOptions)?.addBooleanOption("html5", true)
+    }
 }
