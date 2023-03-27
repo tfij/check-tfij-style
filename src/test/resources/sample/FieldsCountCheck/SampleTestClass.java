f24911cc30b994513e95de74b030a50eb3696822
Tomasz Fijalkowski <3066563+tfij@users.noreply.github.com>
2023-03-27 22:00:14 +0200

    Add fields count check

---
+++ src/test/resources/sample/FieldsCountCheck/SampleTestClass.java
@@ -0,0 +1,15 @@
+private static class SampleTestClass {
+
+    private final static String f1 = "v1";
+    private final static String f2;
+    private final String f3 = "v3";
+    private final String f4;
+    private String f5 = "aa";
+    private String f6;
+
+    private SampleTestClass(String f1, String f2, String f4) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.f4 = f4;
+    }
+}
